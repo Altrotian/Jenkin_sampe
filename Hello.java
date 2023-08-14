8931c3658328ccded7a18794200e78ea5b3df82e
Altrotian <aloksinghw1943c@gmail.com>
2023-08-14 14:55:59 +0530

    adgjk

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{  
     public static void main(String args[]){  
-     System.out.println("It's been a long time. How are they");  
+     System.out.println("It's been a long time. How are they!!");  
     }  
 }  
 
