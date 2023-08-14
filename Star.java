f84dbd57a869ffec8199e3c8fecbbefa49c27a41
Altrotian <aloksinghw1943c@gmail.com>
2023-08-14 12:46:45 +0530

    its star

--- Star.java
+++ Star.java
@@ -1,36 +1,40 @@
 import java.io.*;
 
-// java program for left triangle
-public class GFG {
-	// Function to demonstrate printing pattern
-	public static void StarleftTriangle(int k)
+class GFG
+{
+	// similar to inner for loop
+	public static void printRow(int n, int totalRows)
 	{
-		int a, b;
-
-		// 1st loop
-		for (a = 0; a < k; a++) {
-
-			// nested 2nd loop
-			for (b = 2 * (k - a); b >= 0; b--) {
-				// printing spaces
-				System.out.print(" ");
-			}
-
-			// nested 3rd loop
-			for (b = 0; b <= a; b++) {
-				// printing stars
-				System.out.print("* ");
-			}
-
-			// end-line
-			System.out.println();
+		if(totalRows == 0)
+		{
+			return;
 		}
+		
+		if(totalRows <= n) // condition when to print star
+		{
+			System.out.print(" *");
+		}
+		else // condition when to print space
+		{
+			System.out.print(" ");
+		}
+		printRow(n, totalRows - 1); // moving to next column recursively
 	}
-
-	// Driver Function
-	public static void main(String args[])
+	
+	// similar to outer for loop
+	public static void newRow(int n, int totalRows)
 	{
-		int k = 5;
-		StarleftTriangle(k);
+		if(n == 0)
+		{
+			return;
+		}
+	
+		newRow(n - 1, totalRows);
+		printRow(n, totalRows); // prints stars and space in a single row
+		System.out.print("\n"); // for new Row.....new line
+	}
+	public static void main (String[] args)
+	{
+		GFG.newRow(5, 5); // newRow is static method so no need to create an object of GFG
 	}
 }
